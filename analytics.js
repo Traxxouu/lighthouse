bc0737c4ecce8eca02f458b6c2c2d8a45e90e978
Traxxouu <83843025+Traxxouu@users.noreply.github.com>
2024-09-23 18:28:36 +0200

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,8 +1,1 @@
-// Script d'analyse fictif qui alourdit la page sans réel besoin
-function trackUserActivity() {
-    console.log("Tracking user activity...");
-    // Boucle inutile pour simuler une charge
-    for (let i = 0; i < 500000000; i++) {}
-}
 
-trackUserActivity();
